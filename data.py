3af4939828455eb9dd689242fe304dd1468cab48
gala160494-ops <gala160494@gmail.com>
2026-07-10 20:19:08 +0300

    Добавление файлов проекта

---
+++ data.py
@@ -0,0 +1,12 @@
+
+
+order_body = {
+    "firstName": "Артем",
+    "lastName": "Ворон",
+    "address": "пер.Тихий д.1",
+    "metroStation": "4",
+    "phone": "+79993553535",
+    "rentTime": 5,
+    "deliveryDate": "2026-06-10",
+    "comment": "Привет курьер"
+}
